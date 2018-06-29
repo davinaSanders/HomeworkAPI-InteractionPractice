d72b0d7fbf93a72bec0e4e33ad914e19b411b5fc
davina <davina.kuhnel@gmail.com>
2018-06-29 10:51:38 +0100

    initial set up

---
+++ src/models/movies.js
@@ -0,0 +1,10 @@
+const Request = require("../helpers/request.js");
+const PubSub = require("../helpers/pub_sub.js");
+
+
+const Movies = function(){
+  this.movies = [];
+};
+
+
+module.exports = Movies;
